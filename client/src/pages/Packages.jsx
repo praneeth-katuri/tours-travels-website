7b9dd403d196578ed6d0419344892fc8f4c922ab
praneeth-katuri <praneethkaturi@outlook.com>
2025-05-22 15:55:22 +0530

    feat: Add Packages component

---
+++ client/src/pages/Packages.jsx
@@ -0,0 +1,11 @@
+import PackageHeroSection from "../components/PackageHero/PackageHeroSection";
+import PackageSection from "../components/PackageSection";
+
+export default function Packages() {
+  return (
+    <>
+      <PackageHeroSection />
+      <PackageSection />
+    </>
+  );
+}
